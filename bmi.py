5af8ab1373107c7c8f0d275f170bb93a5a9b4744
Junrrrr <juniorong.24@ichat.sp.edu.sg>
2025-05-28 16:19:02 +0800

    bmi.py updated for Lab3

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
     print("BMI = ", "{:.2f}".format(bmi))
     if (bmi < 18.5):
         print("Under Weight")
+        return -1
     elif (bmi > 25):
         print("Over Weight")
+        return 1
     else:
         print("Normal Weight")
+        return 0
 
 calculate_bmi(height = 1.73, weight = 57)
